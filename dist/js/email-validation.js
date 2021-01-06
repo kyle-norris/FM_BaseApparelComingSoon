702895ff60458e9c2fdb0f5612c2ead2b4d9d02c
cactus <kyle.coding@outlook.com>
2021-01-05 22:12:47 -0500

    Finalized design
    Added error messages to the form. I might revisit this when I learn more about validation to see if there are better ways of doing it.

--- dist/js/email-validation.js
+++ dist/js/email-validation.js
@@ -13,3 +13,21 @@ function ValidateEmail() {
         return false;
     }
 }
+
+
+function validateForm() {
+    var email_text = document.forms["email-form"]["email"].value;
+    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (email_text.length>1 && email_text.match(mailformat)) {
+        document.getElementById('email-form__error').innerHTML = "";
+        return true;
+    } 
+    else {
+        document.getElementById('email-form__error').innerHTML = "Please provide a valid email";
+        document.getElementById('error-img').style.opacity = 1;
+        return false;
+    }
+
+    
+}
